a527380958b1100c533bd4f2f6111d8f0a610ec2
sokolovea <faositb@yandex.ru>
2026-01-06 13:27:26 +0300

    go: leetcode 0144: binary tree preorder traversal

---
+++ go/leetcode/0144_binary_tree_preorder_traversal/solution.go
@@ -0,0 +1,53 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func (treeNode *TreeNode) add(val int) *TreeNode {
+	if treeNode == nil {
+		treeNode = new(TreeNode)
+		treeNode.Val = val
+		return treeNode
+	}
+	treeNodeCopy := treeNode
+	for {
+		if val < treeNode.Val {
+			if treeNode.Left == nil {
+				treeNode.Left = new(TreeNode)
+				treeNode.Left.Val = val
+				break
+			} else {
+				treeNode = treeNode.Left
+			}
+		} else if val > treeNode.Val {
+			if treeNode.Right == nil {
+				treeNode.Right = new(TreeNode)
+				treeNode.Right.Val = val
+				break
+			} else {
+				treeNode = treeNode.Right
+			}
+		} else {
+			break
+		}
+	}
+	return treeNodeCopy
+}
+
+func preorderTraversalInternal(rootPtr *TreeNode, resultSlice *[]int) {
+	if rootPtr == nil {
+		return
+	}
+	preorderTraversalInternal(rootPtr.Left, resultSlice)
+	*resultSlice = append(*resultSlice, (*rootPtr).Val)
+	preorderTraversalInternal(rootPtr.Right, resultSlice)
+}
+
+func preorderTraversal(root *TreeNode) []int {
+	var resultSlice []int = make([]int, 0, 5)
+	preorderTraversalInternal(root, &resultSlice)
+	return resultSlice
+}
---
+++ go/leetcode/0144_binary_tree_preorder_traversal/solution_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var testDataTree = []struct {
+	inputSlice  []int
+	outputSlice []int
+}{
+	{[]int{}, []int{}},
+	{[]int{1}, []int{1}},
+	{[]int{4, 3, 10}, []int{3, 4, 10}},
+	{[]int{8, 4, 2, 1, 5, 14, 12, 13, 16}, []int{1, 2, 4, 5, 8, 12, 13, 14, 16}},
+}
+
+func TestTreePreorder(t *testing.T) {
+	for i, data := range testDataTree {
+		var root *TreeNode = nil
+		for _, number := range data.inputSlice {
+			root = root.add(number)
+		}
+		result := preorderTraversal(root)
+		assert.Equal(t, data.outputSlice, result, fmt.Sprintf("Test case %d failed: result slice %v but must be %v", i, result, data.outputSlice))
+	}
+}
