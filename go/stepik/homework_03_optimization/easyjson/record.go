5d520b878e64abb9972ddf54a8fd099d05515e03
sokolovea <faositb@yandex.ru>
2026-01-29 13:00:44 +0300

    Go: stepik hw3 alpha

---
+++ go/stepik/homework_03_optimization/easyjson/record.go
@@ -0,0 +1,11 @@
+package easyjson
+
+type Record struct {
+	Browsers []string `json:"browsers,omitempty"`
+	Company  string   `json:"company,omitempty"`
+	Country  string   `json:"country,omitempty"`
+	Email    string   `json:"email,omitempty"`
+	Job      string   `json:"job,omitempty"`
+	Name     string   `json:"name,omitempty"`
+	Phone    string   `json:"phone,omitempty"`
+}
---
+++ go/stepik/homework_03_optimization/fast.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"regexp"
+	"strings"
+)
+
+// вам надо написать более быструю оптимальную этой функции
+func FastSearch(out io.Writer) {
+	/*
+		!!! !!! !!!
+		обратите внимание - в задании обязательно нужен отчет
+		делать его лучше в самом начале, когда вы видите уже узкие места, но еще не оптимизировалм их
+		так же обратите внимание на команду в параметром -http
+		перечитайте еще раз задание
+		!!! !!! !!!
+	*/
+	file, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+
+	fileContents, err := io.ReadAll(file)
+	if err != nil {
+		panic(err)
+	}
+
+	r := regexp.MustCompile("@")
+	seenBrowsers := []string{}
+	uniqueBrowsers := 0
+	foundUsers := ""
+
+	lines := strings.Split(string(fileContents), "\n")
+
+	users := make([]Record, 0)
+	for _, line := range lines {
+		var user Record
+		// fmt.Printf("%v %v\n", err, line)
+		err := user.UnmarshalJSON([]byte(line))
+		if err != nil {
+			panic(err)
+		}
+		users = append(users, user)
+	}
+
+	for i, user := range users {
+
+		isAndroid := false
+		isMSIE := false
+
+		for _, browser := range user.Browsers {
+			if strings.Contains(browser, "Android") {
+				isAndroid = true
+				notSeenBefore := true
+				for _, item := range seenBrowsers {
+					if item == browser {
+						notSeenBefore = false
+					}
+				}
+				if notSeenBefore {
+					// log.Printf("SLOW New browser: %s, first seen: %s", browser, user["name"])
+					seenBrowsers = append(seenBrowsers, browser)
+					uniqueBrowsers++
+				}
+			}
+		}
+
+		for _, browser := range user.Browsers {
+			if strings.Contains(browser, "MSIE") {
+				isMSIE = true
+				notSeenBefore := true
+				for _, item := range seenBrowsers {
+					if item == browser {
+						notSeenBefore = false
+					}
+				}
+				if notSeenBefore {
+					// log.Printf("SLOW New browser: %s, first seen: %s", browser, user["name"])
+					seenBrowsers = append(seenBrowsers, browser)
+					uniqueBrowsers++
+				}
+			}
+		}
+
+		if !(isAndroid && isMSIE) {
+			continue
+		}
+
+		// log.Println("Android afnd MSIE user:", user["name"], user["email"])
+		email := r.ReplaceAllString(user.Email, " [at] ")
+		foundUsers += fmt.Sprintf("[%d] %s <%s>\n", i, user.Name, email)
+	}
+
+	fmt.Fprintln(out, "found users:\n"+foundUsers)
+	fmt.Fprintln(out, "Total unique browsers", len(seenBrowsers))
+}
