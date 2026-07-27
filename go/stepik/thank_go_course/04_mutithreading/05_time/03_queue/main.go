3c7dbe1ea66b1ac5f1f365889f7f1f4fbbc08a53
sokolovea <faositb@yandex.ru>
2026-07-27 08:44:26 +0300

    Go: stepik thank go 04_multithreading/05_time/03_queue

--- go/stepik/thank_go_course/04_mutithreading/05_time/03_queue/main.go
+++ go/stepik/thank_go_course/04_mutithreading/05_time/03_queue/main.go
@@ -26,14 +26,14 @@ func (q *Queue) Get(block bool) (int, error) {
 	if q.tail == q.head {
 		if block {
 			<-q.tokens
-			q.tokens <- struct{}{}
 		} else {
 			return 0, ErrEmpty
 		}
+	} else {
+		<-q.tokens
 	}
 	result := q.buffer[q.head]
 	q.head = (q.head + 1) % (q.length + 1)
-	<-q.tokens
 	return result, nil
 }
 
@@ -44,14 +44,14 @@ func (q *Queue) Put(val int, block bool) error {
 	if q.tail == q.length && q.head == 0 || q.head == q.tail+1 {
 		if block {
 			q.tokens <- struct{}{}
-			<-q.tokens
 		} else {
 			return ErrFull
 		}
+	} else {
+		q.tokens <- struct{}{}
 	}
 	q.buffer[q.tail] = val
 	q.tail = (q.tail + 1) % (q.length + 1)
-	q.tokens <- struct{}{}
 	return nil
 }
 
@@ -75,6 +75,7 @@ func main() {
 		fmt.Println("put 12:", err)
 		// put 12: <nil>
 	}()
+
 	res, err := q.Get(true)
 	fmt.Println("get:", res, err)
 	// get: 11 <nil>
